98dbe92ad6c5fb713a39c3c38d183688719fc4ee
valera <valery.sig1@gmail.com>
2018-10-06 21:17:55 +0300

    Initial Commit

---
+++ IOS11-Consentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS11-Consentration
+//
+//  Created by Valery Sigal on 06/10/2018.
+//  Copyright © 2018 Valery Sigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
