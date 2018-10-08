9193f94d5bde6dc14a58acd1eadacd4c36050c92
valera <valery.sig1@gmail.com>
2018-10-09 00:36:02 +0300

    [Extra Credit 1] Added colors the the themes

--- IOS11-Consentration/ViewController.swift
+++ IOS11-Consentration/ViewController.swift
@@ -13,11 +13,15 @@ class ViewController: UIViewController {
     //MARK: Members
     @IBOutlet weak var flipCountLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet var cardButtons: [UIButton]!
     
     var game : Consentration!
     var emojiChoices : [String]!
     var emoji : [Int : String]!
+    
+    var cardBackColor : UIColor!
+    var cardFrontColor : UIColor!
     
     //MARK: Constructors
     override func viewDidLoad() {
@@ -46,10 +50,10 @@ class ViewController: UIViewController {
             let card = game.cards[index]
             if card.isFaceUp {
                 button.setTitle(emoji(for: card), for: UIControlState.normal)
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.backgroundColor = self.cardFrontColor
             } else {
                 button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : self.cardBackColor
             }
         }
         
@@ -66,10 +70,32 @@ class ViewController: UIViewController {
     }
     
     func clear() {
+        let chosenTheme = ViewController.THEMES[ViewController.THEMES.count.getRandomNumUpToIt()]
+        
         self.game = Consentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
-        self.emojiChoices = StaticValues.THEMES[StaticValues.THEMES.count.getRandomNumUpToIt()]
+        self.emojiChoices = chosenTheme.emojis
         self.emoji = [Int : String]()
+        
+        self.view.backgroundColor = chosenTheme.background
+        self.cardBackColor = chosenTheme.cardBack
+        self.cardFrontColor = chosenTheme.cardFront
+        
+        flipCountLabel.textColor = cardBackColor
+        scoreLabel.textColor = cardBackColor
+        newGameButton.backgroundColor = cardBackColor
+        newGameButton.setTitleColor(cardFrontColor, for: UIControlState.normal)
+        
         updateViewFromModel()
     }
+    
+    //MARK: Constants
+    static let THEMES = [
+        (emojis : ["😃", "😁", "😅", "😂", "☺️", "😇", "😍", "😎", "🤨"], background : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cardBack : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), cardFront : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), //Faces
+        (emojis : ["🐶", "🐱", "🐹", "🐨", "🐸", "🐔", "🦄", "🐳", "🦍"], background : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cardBack : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), cardFront : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), //Animals
+        (emojis : ["🍏", "🍎", "🍊", "🍋", "🍉", "🍇", "🍒", "🍑", "🥥"], background : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cardBack : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), cardFront : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), //Fruits
+        (emojis : ["🥞", "🥩", "🍗", "🍖", "🌭", "🍔", "🍟", "🍕", "🥪"], background : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cardBack : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), cardFront : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), //Food
+        (emojis : ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🎱", "🏓", "🏸", "🥅"], background : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cardBack : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), cardFront : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), //Sports
+        (emojis : ["🚗", "🚕", "🚙", "🏎", "🚑", "🚒", "🚐", "🚃", "🚝"], background : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cardBack : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), cardFront : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))  //Transport
+    ]
 }
 
